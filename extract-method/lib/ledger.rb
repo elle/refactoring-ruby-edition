9bc3ab759a0a5d0c2296022ac840b1d477da732d
Elle Meredith <ellemeredith@gmail.com>
2015-11-05 22:06:08 -0500

    Extract method example
    1. Extract method print_banner
    2. Extract method print_details and pass it local variables
    3. Reassigning a Local Variable - outstanding
    4. Use the inject Collection Closure Method (or reduce)
    5. Replace Temp with Query

--- extract-method/lib/ledger.rb
+++ extract-method/lib/ledger.rb
@@ -1,3 +1,8 @@
+# 1. Extract method print_banner
+# 2. Extract method print_details and pass it local variables
+# 3. Reassigning a Local Variable - outstanding
+# 4. Use the inject Collection Closure Method (or reduce)
+# 5. Replace Temp with Query
 class Ledger
   attr_reader :name, :orders
 
@@ -7,17 +12,24 @@ class Ledger
   end
 
   def print_owing
-    outstanding = 0.0
-    # print banner
+    print_banner
+    print_details
+  end
+
+  private
+
+  def print_banner
     puts "*************************"
     puts "***** Customer Owes *****"
     puts "*************************"
-    # calculate outstanding
-    @orders.each do |order|
-      outstanding += order.amount
-    end
-    # print details
-    puts "name: #{@name}"
+  end
+
+  def print_details
+    puts "name: #{name}"
     puts "amount: #{outstanding}"
+  end
+
+  def outstanding
+    orders.map(&:amount).reduce(0.0, :+)
   end
 end
